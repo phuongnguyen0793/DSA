ef3601710081f33ea6b64d2c357bf7c6a715d0fc
Phuong Nguyen <90946003+phuongnguyen0793@users.noreply.github.com>
2025-08-02 17:56:12 +0700

    Time: 3 ms (61.66%), Space: 44.8 MB (89.62%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seens = new HashMap<>();
+        for(int i = 0; i < nums.length; ++i){
+            int complement = target - nums[i];
+            if(seens.containsKey(complement)) {
+                return new int[] {seens.get(complement), i};
+            }
+            seens.put(nums[i], i);
+        }
+        return new int[] {};
+ 
+        // for (int i = 0; i < nums.length; ++i) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return new int[] { i, j };
+        //         }
+        //     }
+        // }
+        // return new int{};
+    }
+}

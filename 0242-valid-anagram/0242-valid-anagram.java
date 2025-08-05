e57905582249d211212fbf94df902bddf48082de
Phuong Nguyen <90946003+phuongnguyen0793@users.noreply.github.com>
2025-08-06 03:13:05 +0700

    Time: 1 ms (99.59%), Space: 43.1 MB (81.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,9 +5,12 @@ class Solution {
         }
 
         int[] charCount = new int[26];
-        for (int i = 0; i < s.length(); ++i) {
-            ++charCount[s.charAt(i) - 'a'];
-            --charCount[t.charAt(i) - 'a'];
+        for (char c : s.toCharArray()) {
+            ++charCount[c - 'a'];
+        }
+
+        for (char c : t.toCharArray()) {
+            --charCount[c - 'a'];
         }
 
         for (int i = 0; i < charCount.length; ++i) {

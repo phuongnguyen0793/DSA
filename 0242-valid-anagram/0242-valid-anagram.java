873eac9aaf4abb4120842a8b7da1d4ce5053d305
Phuong Nguyen <90946003+phuongnguyen0793@users.noreply.github.com>
2025-08-06 03:05:07 +0700

    Time: 2 ms (97.88%), Space: 43.1 MB (76.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    int sLen = s.length(), tLen = t.length();
-    if (sLen != tLen) {
-      return false;
-    }
+        int sLen = s.length(), tLen = t.length();
+        if (sLen != tLen) {
+            return false;
+        }
 
-    HashMap<Character, Integer> expectedCount = new HashMap<>();
-    for (char c : s.toCharArray()) {
-      expectedCount.put(c, expectedCount.getOrDefault(c, 0) + 1);
-    }
+        int[] charCount = new int[26];
+        for (char c : s.toCharArray()) {
+            ++charCount[c - 'a'];
+        }
 
-    HashMap<Character, Integer> tCount = new HashMap<>();
-    for (char c : t.toCharArray()) {
-      tCount.put(c, tCount.getOrDefault(c, 0) + 1);
-    }
+        for (char c : t.toCharArray()) {
+            --charCount[c - 'a'];
+        }
 
-    return expectedCount.equals(tCount);
-  }
+        for (int count : charCount) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
